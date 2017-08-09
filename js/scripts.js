e08040b2277909502d2231147eaa2ea40e8ce849
Debbie Michel and Linda Luu <linda@>
2017-08-09 16:22:14 -0700

    add forms to js and output

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,34 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function(event) {
-    var blanks = ["person", "fav-food", "fav-music", "date",];
+  // $("#blanks form").submit(function(event) {
+  //   var blanks = ["person", "fav-food", "fav-music", "date",];
+  //
+  //   blanks.forEach(function(blank) {
+  //     var userInput = $("input#" + blank).val();
+  //     $("." + blank).text(userInput).val();
 
-    blanks.forEach(function(blank) {
-      var userInput = $("input#" + blank).val();
-      $("." + blank).text(userInput).val();
-    });
+
+    $("#formOne").submit(function(event) {
+      var personInput = $("input#person").val();
+      var dateInput = $("input#date").val();
+      var fav-foodInput= $("input#fav-food").val();
+      var fav-musicInput = $("input#fav-music").val();
+      var fav-colorInput = $("input#fav-color").val();
+
+
+      $(".person").text(personInput);
+      $(".date").text(dateInput);
+      $(".fav-food").text(fav-foodInput);
+      $(".fav-music").text(fav-musicInput);
+      $(".fav-color").text(fav-colorInput);
+
+
 
     $("#story").show();
 
-    var dob = $("#born").val();
-    var flavor = $("input:radio[name=flavor]:checked").val();
-    var beverage = $("#beverage").val();
-    var favoriteColor = $("#color").val();
+    // var dob = $("#born").val();
+    // var flavor = $("input:radio[name=flavor]:checked").val();
+    // var beverage = $("#beverage").val();
+    // var favoriteColor = $("#color").val();
 
     event.preventDefault();
   });
